80b6c1442c14ab54e9413eccf17f25fcb6e1783a
erik tolentino <tolentino3575@gmail.com>
2016-03-17 15:37:56 -0700

    Add a map

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,3 +1,24 @@
-$(document).ready (function(event){
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+    center: {lat: 45.523, lng: -122.676},
+    zoom: 8,
+    mapTypeId:google.maps.MapTypeId.TERRAIN
+  });
 
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+}
+
+
+$(document).ready (function(){
+  initMap();
 });
+
+
+// https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
