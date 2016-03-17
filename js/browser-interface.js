9c017fd17ec33ecf3e55a30bd166cd107f97a617
erik tolentino <tolentino3575@gmail.com>
2016-03-17 16:23:20 -0700

    Get search on map working

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,19 +1,4 @@
-function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {lat: 45.523, lng: -122.676},
-    zoom: 8,
-    mapTypeId:google.maps.MapTypeId.TERRAIN
-  });
-
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-}
+var initMap = require('./../js/map.js').initMap;
 
 
 $(document).ready (function(){
